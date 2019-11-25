3c36c4a269367ac48ec5b8d1ab1ba7f78dd2b4fd
Alex Wolf <alexrudiwolf@gmail.com>
2019-11-25 00:38:25 -0500

    added initial source code

---
+++ Tasker/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tasker
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Task> Tasks { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
