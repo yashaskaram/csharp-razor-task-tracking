740319f1ca5ea80f871a684c4712a02fa5d1ffb2
Alex Wolf <alex.wolf@spectrumhealth.org>
2020-01-27 13:34:20 -0500

    updated regex

--- Tasker.Tests/DisplayingDataOnHomePage/DisplayingTheTasksTests.cs
+++ Tasker.Tests/DisplayingDataOnHomePage/DisplayingTheTasksTests.cs
@@ -24,7 +24,7 @@ namespace Tasker.Tests.DisplayingDataOnHomePage
                 file = streamReader.ReadToEnd();
             }
 
-            var pattern = @"<\s*?table\s*?>\s*?@foreach\s*?[(]\s*?(var|Task)\s*task\s*in\s*Model.Tasks\s*?[)]\s*?{\s*?<\s*?[tT][rR]\s*?>\s*?<[tT][dD]>\s*?@task.Id\s*?<\s*?\/\s*?[tT][dD]\s*?>\s*?\s*?<[tT][dD]>\s*?@task.Title\s*?<\s*?\/\s*?[tT][dD]\s*?>\s*?\s*?<[tT][dD]>\s*?@task.Description\s*?<\s*?\/\s*?[tT][dD]\s*?>\s*?\s*?<[tT][dD]>\s*?@task.Priority\s*?<\s*?\/\s*?[tT][dD]\s*?>\s*?<\/\s*?[tT][rR]\s*?>\s*?}\s*?<\s*?\/table\s*?>";
+            var pattern = @"<\s*?table\s*?>[\s\S]*?@foreach\s*?[(]\s*?(var|Task)\s*task\s*in\s*Model.Tasks\s*?[)]\s*?{\s*?<\s*?[tT][rR]\s*?>\s*?<[tT][dD]>\s*?@task.Id\s*?<\s*?\/\s*?[tT][dD]\s*?>\s*?\s*?<[tT][dD]>\s*?@task.Title\s*?<\s*?\/\s*?[tT][dD]\s*?>\s*?\s*?<[tT][dD]>\s*?@task.Description\s*?<\s*?\/\s*?[tT][dD]\s*?>\s*?\s*?<[tT][dD]>\s*?@task.Priority\s*?<\s*?\/\s*?[tT][dD]\s*?>\s*?<\/\s*?[tT][rR]\s*?>\s*?}\s*?<\s*?\/table\s*?>";
             var rgx = new Regex(pattern);
             Assert.True(rgx.IsMatch(file), "`Index.cshtml` was found, but does not appear to contain a `table` with a `foreach` loop that creates rows and columns for the `task` items.");
         }
