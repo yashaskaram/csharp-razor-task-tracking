5b6915b78f1c6f9d3b959ff8bc58b881a3036877
yashaskaram <contactvikas24@gmail.com>
2022-09-27 16:39:36 -0500

    Build the Task Form

--- Tasker/Pages/CreateTask.cshtml.cs
+++ Tasker/Pages/CreateTask.cshtml.cs
@@ -9,8 +9,16 @@ namespace Tasker.Pages
 {
     public class CreateTaskModel : PageModel
     {
+        [BindProperty]
+        public Task NewTask { get; set; }
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage("Index");
         }
     }
 }
